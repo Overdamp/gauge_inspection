1cfda8d679de38b8533de1d809d0d1d30679cf08
Overdamp <thanatchai.wong@kmutt.ac.th>
2026-05-08 15:12:27 +0700

    program updated

--- main.py
+++ main.py
@@ -28,8 +28,8 @@ def main():
         logger.error(f"Failed to load models: {e}")
         return
 
-    input_folder = "/home/engineer00/winworkspace/PTTEP/Objectdetection/crop_gauge_result/analog-gauge"
-    output_folder = "/home/engineer00/winworkspace/PTTEP/AnalogGaugeReading/gauge-win-v3/results"
+    input_folder = "images/analog-gauge"
+    output_folder = "results"
 
     os.makedirs(input_folder, exist_ok=True)
     os.makedirs(output_folder, exist_ok=True)
